86f476710345856ffc575c359d8de8825e53aff2
Maryam-Dollet <105509172+Maryam-Dollet@users.noreply.github.com>
2022-12-05 23:52:46 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -171,6 +171,8 @@ with st.sidebar:
 
 if selected == "Home":
     st.title('Introduction of the Database')
+    st.markdown("This database is a study made on the consumption of various drugs, the survey asked diffents individuals from different gender, age, education, country and ethnicity")
+    st.markdown("In this survey, the consuption of 19 drugs was asked")
 
     table = ['ID','Age','Gender','Education','Country','Ethnicity','Nscore','Escore','Oscore','Ascore','Cscore','Impulsive','SS','Alcohol','Amphet','Amyl','Benzos','Caff','Cannabis','Choc','Coke','Crack','Ecstasy','Heroin','Ketamine','Legalh','LSD','Meth','Mushrooms','Nicotine','Semer','VSA']
     df_init = pd.read_csv("drug_consumption.data", sep =',', header=None,names= table)
